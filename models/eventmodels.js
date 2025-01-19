338b8fec704955b803bdaa780111bd1f610f2a95
141Soft <161060240+141Soft@users.noreply.github.com>
2025-01-19 19:36:38 +0000

    Refactors postEvent, better error handling, using transactions

--- models/eventmodels.js
+++ models/eventmodels.js
@@ -1,51 +1,41 @@
 import { db } from '../db.js'
 
 //This needs more robust error handling, should validate arguments
-//Consider doing a map + promiseAll for img/tagarray queries to make async
 const postEvent = async (name, date, desc, imgarray, tagarray) => {
 
-    const query = 
-    `
-    INSERT INTO events (eventName, eventDate, eventDesc)
-    VALUES(?, ?, ?)
-    RETURNING *;
-    `;
-
     let id;
+    let connection;
 
-    try{
-        const [fields] = await db.pool.query(query,[name, date, desc]);
+    try {
+        connection = await db.pool.getConnection();
+        await connection.beginTransaction();
+
+        const [fields] = await connection.query(
+            'INSERT INTO events (eventName, eventDate, eventDesc) VALUES(?, ?, ?) RETURNING *',
+            [name, date, desc]);
         id = fields[0].id;
+
+        for(const tag of tagarray) {
+            await connection.query(
+                'INSERT INTO eventTags (eventID, eventTag) VALUES (?, ?)',
+                [id, tag]
+            );
+        }
+
+        for(const img of imgarray) {
+            await connection.query(
+                'INSERT INTO eventImages (eventID, imgurl) VALUES (?, ?)',
+                [id, img]
+            );
+        }
+
+        await connection.commit();
+
     } catch(error) {
+        if(connection){ await connection.rollback() };
         console.error(error);
         throw error;
-    }
-
-    for(const tag of tagarray){
-        const query = 
-        `
-        INSERT INTO eventTags (eventID, eventTag)
-        VALUES(?, ?);
-        `;
-        try{
-            await db.pool.query(query,[id, tag]);
-        } catch(error) {
-            console.error(error);
-            throw error;
-        }
-    }
-
-    for(const img of imgarray){
-        const query = 
-        `
-        INSERT INTO eventImages (eventID, imgurl)
-        VALUES(?, ?);
-        `
-        try{
-            await db.pool.query(query, [id, img]);
-        } catch(error) {
-            console.error(error);
-            throw error;
-        }
+    } finally {
+        if(connection) { connection.release() }
     }
 }
