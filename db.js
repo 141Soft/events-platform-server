cbc08a9f9784e559639d2f109d779c1663ebd374
141Soft <161060240+141Soft@users.noreply.github.com>
2025-01-19 16:24:06 +0000

    Converts DBHelper class to be promise based, implements better error handling

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-import mysql2 from 'mysql2';
-import fs from 'fs';
+import mysql2 from 'mysql2/promise';
+import fs from 'fs/promises';
 import { parseSQL } from './utils/parsers.js';
 
 const MYSQL_CONF = {
@@ -17,7 +17,7 @@ class DBHelper {
         return this;
     }
 
-    openPool(config) {
+    async openPool(config) {
         return this.pool = mysql2.createPool(config);
     }
 
@@ -27,43 +27,91 @@ class DBHelper {
     }
 
     //Allows multiple operations to be carried out on the same connection before releasing
-    reserveConnection(callback) {
-        this.pool.getConnection((error, connection) => {
-            if(error){ 
-                console.error(error) 
-                return callback(error);
+    async reserveConnection(callback) {
+
+        let connection = null;
+
+        try{
+            connection = await this.pool.getConnection();
+            return await callback(connection);
+        } catch(error) {
+            console.error(error);
+            throw error;
+        } finally {
+            if(connection){
+                connection.release();
             }
-            callback(connection)
-            connection.release();
-        })
+        }
     }
 
-    seed(schemaPath, seedPath) {
+    async schema(schemaPath) {
         if(this.pool === null){
-            console.log('Open a pool before attempting to seed!');
+            console.error('Connection pool not initialized.');
+            return false
+        };
+        if(!schemaPath){
+            console.error('Missing required path: Schema.');
+            return false
+        };
+
+        let connection = null;
+
+        try{
+            connection = await this.pool.getConnection();
+            const queries = parseSQL(await fs.readFile(schemaPath));
+
+            console.log(`Implementing database Schema with ${queries.length} queries`)
+            for(const query of queries){
+                const result = await this.pool.query(query);
+                console.log(result);
+            }
+            console.log('Schema complete.');
+            return true;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        } finally {
+            if(connection) {
+                connection.release();
+            }
+        }
+    }
+
+    async seed(schemaPath, seedPath) {
+        if(this.pool === null){
+            console.error('Connection pool not initialized.');
             return false
         };
         if(!schemaPath || !seedPath){
-            console.log('Please specify schema and seed.');
+            console.error('Missing required paths: Schema and Seed.');
             return false
         };
 
-        //Reserving a connection here for potential multiple steps
-        this.pool.getConnection((error, connection) => {
-            if(error){ console.error(error) };
+        let connection = null;
+        try{
+            connection = await this.pool.getConnection();
 
-            const schema = parseSQL(fs.readFileSync(schemaPath));
-            const seed = parseSQL(fs.readFileSync(seedPath));
-            const queries = [...schema, ...seed];
+            const [schemaFile, seedFile] = await Promise.all([
+                fs.readFile(schemaPath),
+                fs.readFile(seedPath)
+            ]);
+            const queries = [...parseSQL(schemaFile), ...parseSQL(seedFile)];
 
-            queries.forEach((query) => {
-                connection.query(query, (error, results) => {
-                    if(error){ console.error(error) }
-                })
-                
-            })
-            connection.release();
-        })
+            console.log(`Seeding database with ${queries.length} queries...`);
+            for(const query of queries){
+                const result = await connection.query(query);
+                console.log(result);
+            }
+            console.log('Seeding complete!');
+            return true;
+        } catch(error) {
+            console.error(error);
+            throw error;
+        } finally {
+            if(connection){
+                connection.release();
+            }
+        }
     }
 }
 
---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS eventsPlatform;
+
+USE eventsPlatform;
+
+DROP TABLE IF EXISTS events, eventTags, eventImages;
+
+CREATE TABLE IF NOT EXISTS events (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    eventName VARCHAR(100) NOT NULL,
+    eventDate DATETIME NOT NULL,
+    eventDesc TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS eventTags (
+    eventID INT NOT NULL,
+    eventTag VARCHAR(20) NOT NULL,
+    PRIMARY KEY(eventID, eventTag)
+);
+
+CREATE TABLE IF NOT EXISTS eventImages (
+    eventID INT NOT NULL,
+    imgurl VARCHAR(100) NOT NULL,
+    PRIMARY KEY(eventID, imgurl)
+);
